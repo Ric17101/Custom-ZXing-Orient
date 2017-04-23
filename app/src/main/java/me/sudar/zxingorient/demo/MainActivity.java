244dcf9888583a955efca1d6b3dd99c7a261836b
Ric17101 <ric17101raguine@gmail.com>
2017-04-23 15:32:16 +0800

    My ACtivity Commit

--- app/src/main/java/me/sudar/zxingorient/demo/MainActivity.java
+++ app/src/main/java/me/sudar/zxingorient/demo/MainActivity.java
@@ -10,26 +10,18 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.mikepenz.aboutlibraries.Libs;
-import com.mikepenz.aboutlibraries.LibsBuilder;
-import com.mikepenz.devicon_typeface_library.DevIcon;
-import com.mikepenz.iconics.IconicsDrawable;
 
 import me.sudar.zxingorient.Barcode;
 import me.sudar.zxingorient.R;
 import me.sudar.zxingorient.ZxingOrient;
 import me.sudar.zxingorient.ZxingOrientResult;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private TextView resultTextView;
@@ -45,54 +37,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         resultTextView = (TextView) findViewById(R.id.result_text_view);
         shareEditText = (EditText) findViewById(R.id.encodeEditText);
-        findViewById(R.id.button_1).setOnClickListener(this);
         findViewById(R.id.button_2).setOnClickListener(this);
-        findViewById(R.id.button_3).setOnClickListener(this);
-        findViewById(R.id.button_4).setOnClickListener(this);
         findViewById(R.id.button_5).setOnClickListener(this);
 
-        TextView textView =(TextView)findViewById(R.id.github_link);
-        textView.setClickable(true);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
-        String text = "Check out the <a href='https://github.com/SudarAbisheck/ZXing-Orient'>Github Repo !!</a>";
-        textView.setText(Html.fromHtml(text));
-
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
-                requestCameraPermission();
+            requestCameraPermission();
         }
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.button_1:
-                    new ZxingOrient(MainActivity.this).initiateScan();
-                break;
+        switch (v.getId()) {
             case R.id.button_2:
                 new ZxingOrient(MainActivity.this)
-                        .setInfo("QR code Scanner with UI customization")
-                        .setToolbarColor("#c099cc00")
-                        .setInfoBoxColor("#c099cc00")
+                        .setInfo("QR code Scanner")
+                        //.setToolbarColor("#c099cc00")
+                        //.setInfoBoxColor("#c099cc00")
                         .setBeep(false)
+                        .setVibration(true)
+                        .setIcon(R.drawable.custom_icon)
                         .initiateScan(Barcode.QR_CODE);
                 break;
-            case R.id.button_3:
-                    new ZxingOrient(MainActivity.this)
-                            .setIcon(R.drawable.custom_icon)
-                            .setVibration(true)
-                            .initiateScan(Barcode.ONE_D_CODE_TYPES);
-                break;
-            case R.id.button_4:
-                new ZxingOrient(MainActivity.this)
-                        .setIcon(R.drawable.custom_icon)
-                        .setInfo("Scans 2D barcodes")
-                        .initiateScan(Barcode.TWO_D_CODE_TYPES);
-                break;
-
             case R.id.button_5:
-                if(shareEditText.getText().length() == 0)
+                if (shareEditText.getText().length() == 0)
                     new ZxingOrient(MainActivity.this).shareText("https://github.com/SudarAbisheck/ZXing-Orient");
                 else
                     new ZxingOrient(MainActivity.this).shareText(shareEditText.getText().toString());
@@ -106,35 +75,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (scanResult != null && scanResult.getContents() != null) {
             resultTextView.setText(scanResult.getContents());
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        toolbar.getMenu()
-                .findItem(R.id.about_icon)
-                .setIcon(new IconicsDrawable(this, DevIcon.Icon.dev_github_plain).actionBar().colorRes(R.color.iconColor));
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.about_icon) {
-            new LibsBuilder()
-                    .withFields(R.string.class.getFields())
-                    .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
-                    .withActivityTitle(getApplication().getString(R.string.title_about))
-                    .withLicenseShown(true)
-                    .withAboutIconShown(true)
-                    .withAboutVersionShown(true)
-                    .withAboutDescription("This is demo app displaying the features of <b>ZXing-Orient</b> library.")
-                    .start(this);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void requestCameraPermission() {
